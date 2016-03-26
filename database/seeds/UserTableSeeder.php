7e95e64d71c200aa2cce7f1c1b8ed0c49e7365bc
dburgosm <dburgosm@gmail.com>
2016-03-26 18:16:09 -0300

    Update UserTableSeeder.php

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -8,6 +8,6 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         DB::collection('users')->delete();
-        DB::collection('users')->insert(['name' => 'David Burgos M', 'email' => 'dburgosm@gmail.com', 'password' => 'pandad']);
+        DB::collection('users')->insert(['name' => 'David Burgos M', 'email' => 'dburgosm@test.com', 'password' => 'pandad']);
     }
 }
